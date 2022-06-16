8ca2c2c67d6e6ddc0ea88eac06a3bd654e7d7206
SwapnilPadave <Spadave7@gmail.com>
2022-06-16 17:23:47 +0530

    MyJwtauth

---
+++ JwtToken/Models/UserConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtApp.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { Username = "Swapnil", EmailAddress = "swapnil@gmail.com", Password = "Password", GivenName = "Swapi", Surname = "Padave", Role = "Administrator" },
+            new UserModel() { Username = "admin", EmailAddress = "Swapi@email.com", Password = "admin", GivenName = "add", Surname = "Admin", Role = "Seller" },
+        };
+    }
+}
